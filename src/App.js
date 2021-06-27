67ca0d4d67f20374e12fa3c4f1c842c5bff7226e
Wessam Abdelwahab <41477970+wessamabdelwahab@users.noreply.github.com>
2021-06-27 15:24:00 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	    <p>
+          Certificate in DevOps.
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          <code><li>CSDO1000-Introduction to DevOps</li></code>
+          <code><li>CSDO1010-The DevOps Toolchain in Practice</li></code>
+          <code><li>CSDO1020-Next Generation DevOps</li></code>
+        </ul> 
+        <a
+          className="App-link"
+          href="https://continue.yorku.ca/programs/certificate-in-devops/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn DevOps with York University School of Continuing Studies
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
